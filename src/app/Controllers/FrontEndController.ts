287f3d0aac82efdd5580e5022b546eadc4b56707
Abhineet Tandon <abhineettandon@live.com>
2020-09-01 13:10:27 +0530

    added product category list

--- src/app/Controllers/FrontEndController.ts
+++ src/app/Controllers/FrontEndController.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 
 import { Product } from "../Models/Product";
+import { ProductCategory } from "../Models/ProductCategory";
 import { VisibilityStatus } from "../../types/VisibilityStatusEnum";
+import { Schema } from "mongoose";
 
 export class FrontEndController {
   static getProducts = async (
@@ -25,6 +27,33 @@ export class FrontEndController {
       return res
         .status(500)
         .json({ message: "Cannot get products. Something went wrong." });
+    }
+  };
+
+  static getCategories = async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
+    const { id } = req.params;
+
+    try {
+      const categories = await ProductCategory.find({
+        productId: (id as unknown) as Schema.Types.ObjectId,
+        $or: [
+          {
+            status: VisibilityStatus.PUBLISHED,
+          },
+          {
+            status: VisibilityStatus.LOCKED,
+          },
+        ],
+      });
+
+      return res.json({ data: { categories } });
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ message: "Cannot query categories. Something went wrong." });
     }
   };
 }
--- src/routes/frontEnd.routes.ts
+++ src/routes/frontEnd.routes.ts
@@ -5,5 +5,6 @@ import { FrontEndController } from "../app/Controllers/FrontEndController";
 const frontEnd: Router = Router();
 
 frontEnd.get("/products", FrontEndController.getProducts);
+frontEnd.get("/products/:id/categories", FrontEndController.getCategories);
 
 export default frontEnd;
